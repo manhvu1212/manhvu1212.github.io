d11ec4269fa8fc58b42d6e78fd086696349c52d7
Vu Nguyen Manh <vunm@pixelz.com>
2019-12-24 18:30:31 +0700

    so sanh uu dai

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $('#tinhLai').submit(function (event) {
 
         let tongTienPhaiTra = gocPhaiTra + laiPhaiTra;
         tienGocConLai -= gocPhaiTra;
-        tongLaiPhaiTra += laiPhaiTra
+        tongLaiPhaiTra += laiPhaiTra;
         $('#keHoachTraLai').append(
             "<tr> \
                 <td>" + i + "</td> \
@@ -36,6 +36,59 @@ $('#tinhLai').submit(function (event) {
             <td>" + formatNumber(tongLaiPhaiTra) + "</td> \
             <td>" + formatNumber(soTienCanVay + tongLaiPhaiTra) + "</td> \
             <td></td> \
+        </tr>");
+});
+
+$('#soSanhLai').submit(function (event) {
+    event.preventDefault();
+    $('#keHoachTraLai').empty();
+
+    let soTienCanVay = parseNumber($('#soTienCanVay').val());
+    let thoiGianVay = $('#thoiGianVay').val();
+    let laiSuatUocTinh = $('#laiSuatUocTinh').val();
+    let laiSuatUuDai1 = $('#laiSuatUuDai1').val();
+    let thoiGianUuDai1 = $('#thoiGianUuDai1').val();
+    let laiSuatUuDai2 = $('#laiSuatUuDai2').val();
+    let thoiGianUuDai2 = $('#thoiGianUuDai2').val();
+
+    let thoiGianSoSanh = Math.max(thoiGianUuDai1, thoiGianUuDai2);
+
+    let tienGocConLai = soTienCanVay;
+    let tongLaiPhaiTra1 = 0;
+    let tongLaiPhaiTra2 = 0;
+
+    for (let i = 1; i <= thoiGianSoSanh; i++) {
+        let gocPhaiTra = Math.floor(soTienCanVay / thoiGianVay);
+        let laiPhaiTra1 = Math.floor(tienGocConLai * laiSuatUocTinh / 100 / 12);
+        let laiPhaiTra2 = Math.floor(tienGocConLai * laiSuatUocTinh / 100 / 12);
+        if (i <= thoiGianUuDai1) {
+            laiPhaiTra1 = Math.floor(tienGocConLai * laiSuatUuDai1 / 100 / 12);
+        }
+        if (i <= thoiGianUuDai2) {
+            laiPhaiTra2 = Math.floor(tienGocConLai * laiSuatUuDai2 / 100 / 12);
+        }
+
+        tienGocConLai -= gocPhaiTra;
+        tongLaiPhaiTra1 += laiPhaiTra1;
+        tongLaiPhaiTra2 += laiPhaiTra2;
+        $('#keHoachTraLai').append(
+            "<tr> \
+                <td>" + i + "</td> \
+                <td>" + formatNumber(gocPhaiTra) + "</td> \
+                <td>" + formatNumber(laiPhaiTra1) + "</td> \
+                <td>" + formatNumber(gocPhaiTra + laiPhaiTra1) + "</td> \
+                <td>" + formatNumber(laiPhaiTra2) + "</td> \
+                <td>" + formatNumber(gocPhaiTra + laiPhaiTra2) + "</td> \
+            </tr>");
+    }
+    $('#keHoachTraLai').append(
+        "<tr> \
+            <td>Tổng</td> \
+            <td>" + formatNumber(soTienCanVay) + "</td> \
+            <td>" + formatNumber(tongLaiPhaiTra1) + "</td> \
+            <td>" + formatNumber(soTienCanVay + tongLaiPhaiTra1) + "</td> \
+            <td>" + formatNumber(tongLaiPhaiTra2) + "</td> \
+            <td>" + formatNumber(soTienCanVay + tongLaiPhaiTra2) + "</td> \
         </tr>");
 });
 
